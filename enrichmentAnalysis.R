957b3377686f5653a27227c4cd7b2b3cacc630b4
benja0x40 <benjaminolivierleblanc@gmail.com>
2015-07-25 16:04:02 +0200

    Updated demo script
    [enrichmentAnalysis.R]
    + Improved source code structure and comments
    + Added plot to control selected probes
    + Removed unused code

--- enrichmentAnalysis.R
+++ enrichmentAnalysis.R
@@ -1,31 +1,37 @@
 # =============================================================================.
 # Load R package
+# -----------------------------------------------------------------------------.
 library("MRA.TA")
 
 # =============================================================================.
+# Import microarray data
+# -----------------------------------------------------------------------------.
 # Read predefined experimental setup
-experiment.design <- read.delim("Experiment_Design/GSE23887_dm6.txt", stringsAsFactors=F)
+experiment.design <- read.delim(
+  "Experiment_Design/GSE23887_dm6.txt", stringsAsFactors=F
+)
 
-# Read raw data
+# Read raw data (two replicates)
 array.data.format <- file.formats$nimblegen.pair
-r1.ct <- readData(experiment.design$SAMPLE_PATH[1], array.data.format)
-r1.4C <- readData(experiment.design$SAMPLE_PATH[2], array.data.format)
-r2.ct <- readData(experiment.design$SAMPLE_PATH[3], array.data.format)
-r2.4C <- readData(experiment.design$SAMPLE_PATH[4], array.data.format)
+r1.ct <- readData(experiment.design$SAMPLE_PATH[1], array.data.format) # control
+r1.4C <- readData(experiment.design$SAMPLE_PATH[2], array.data.format) # 4C
+r2.ct <- readData(experiment.design$SAMPLE_PATH[3], array.data.format) # control
+r2.4C <- readData(experiment.design$SAMPLE_PATH[4], array.data.format) # 4C
 
-# =============================================================================.
-# Read updated array design
+# Read updated and cleaned array design annotations
 array.design.path <- unique(experiment.design$DESIGN_PATH)
 array.design.format <- file.formats$nimblegen.updated
 array.design <- readData(array.design.path, array.design.format)
 
-# Match array data to updated design
+# Match array data to updated design using probe identifiers
 r1.4C <- r1.4C[match(array.design$PROBE_ID, r1.4C$PROBE_ID),]
 r1.ct <- r1.ct[match(array.design$PROBE_ID, r1.ct$PROBE_ID),]
 r2.4C <- r2.4C[match(array.design$PROBE_ID, r2.4C$PROBE_ID),]
 r2.ct <- r2.ct[match(array.design$PROBE_ID, r2.ct$PROBE_ID),]
 
 # =============================================================================.
+# Import coordinates of restriction fragments and match with probe coordinates
+# -----------------------------------------------------------------------------.
 # Read precomputed restriction fragments
 fragments <- readData(
   "Processed_Data/RestrictionMap/dm6_DpnII_Fragments.txt.gz",
@@ -54,35 +60,40 @@ fragments.grg <- GRanges(
   )
 )
 
-# Match probes to fragments using the genomic intervals
+# Match probes to fragments using respective genomic intervals
 probes.grg <- matchProbesToFragments(probes.grg, fragments.grg)
 
 # Reorder data consistently with matched probes 
 idx <- match(names(probes.grg), array.design$PROBE_ID)
-array.design <- array.design[idx,]
+array.design <- array.design[idx,] # For consistency with data and probes.grg
 r1.ct <- r1.ct[idx,]; r1.4C <- r1.4C[idx,]
 r2.ct <- r2.ct[idx,]; r2.4C <- r2.4C[idx,]
 
 # =============================================================================.
+# Normalization procedure
+# -----------------------------------------------------------------------------.
 # Compute raw A and M values in each replicate
-r1.A <- (log2(r1.4C$PM) + log2(r1.ct$PM))/2
-r1.M <- (log2(r1.4C$PM) - log2(r1.ct$PM))
-r2.A <- (log2(r2.4C$PM) + log2(r2.ct$PM))/2
-r2.M <- (log2(r2.4C$PM) - log2(r2.ct$PM))
+r1.A <- (log2(r1.4C$PM) + log2(r1.ct$PM))/2 # average
+r1.M <- (log2(r1.4C$PM) - log2(r1.ct$PM))   # log2 ratio
+r2.A <- (log2(r2.4C$PM) + log2(r2.ct$PM))/2 # average
+r2.M <- (log2(r2.4C$PM) - log2(r2.ct$PM))   # log2 ratio
 
 # Apply background bias correction and lowess normalization to each replicate
-res <- normalizeArrayData( # Replicate 1
+# Replicate 1
+res <- normalizeArrayData(
   r1.A, r1.M, name="r1_norm", plots=TRUE
 )
-r1.A <- res$A; r1.M <- res$M
-res <- normalizeArrayData( # Replicate 2
+r1.A <- res$A; r1.M <- res$M # Update A and M with normalized values
+# Replicate 2
+res <- normalizeArrayData(
   r2.A, r2.M, name="r2_norm", plots=TRUE
 )
-r2.A <- res$A; r2.M <- res$M
+r2.A <- res$A; r2.M <- res$M # Update A and M with normalized values
 
 # =============================================================================.
-# Step by step normalization procedure
-if(F) {
+# Normalization procedure (Step by step alternative)
+# -----------------------------------------------------------------------------.
+if(T) {
   # Compute raw A and M values in each replicate
   r1.A <- (log2(r1.4C$PM) + log2(r1.ct$PM))/2
   r1.M <- (log2(r1.4C$PM) - log2(r1.ct$PM))
@@ -90,24 +101,30 @@ if(F) {
   r2.M <- (log2(r2.4C$PM) - log2(r2.ct$PM))
   
   # Estimate background bias
+  pdf("backgroundBiasEstimation.pdf", width=5, height=5)
   bb.r1 <- backgroundBiasEstimation(r1.A, r1.M, plots = T)
   bb.r2 <- backgroundBiasEstimation(r2.A, r2.M, plots = T)
+  dev.off()
 
   # Correct background bias
   res <- backgroundBiasCorrection(r1.A, r1.M, theta=bb.r1)
   r1.A <- res$x; r1.M <- res$y
   res <- backgroundBiasCorrection(r2.A, r2.M, theta=bb.r2)
   r2.A <- res$x; r2.M <- res$y
-  
+
   # Apply lowess correction
+  pdf("lowessCorrection.pdf", width=5, height=5)
   res <- lowessCorrection(r1.A, r1.M, lowess.f=0.2, plots = T)
   r1.A <- res$x; r1.M <- res$y
   res <- lowessCorrection(r2.A, r2.M, lowess.f=0.2, plots = T)
   r2.A <- res$x; r2.M <- res$y
+  dev.off()
 }
 
 # =============================================================================.
-# Identify probes likely associated with good/poor signal based on raw data
+# Probe selection
+# -----------------------------------------------------------------------------.
+# Identify probes likely associated with best/problematic signals with raw data
 pdf("probesFitering.r1.pdf", width=5, height=5)
 r1.EQ <- enrichmentQuality(r1.ct$PM, r1.4C$PM, plots=T) # Replicate 1
 dev.off()
@@ -123,27 +140,38 @@ pdf("probeDistanceControls_r2.M.pdf", width=5, height=5)
 plotProbeDistanceControls(r2.M, rnk=probes.grg$RF_RANK, dis=probes.grg$RF_DIST, dlim=c(-1500, 1500), QF=r2.EQ)
 dev.off()
 
-#TODO
-brks <- 100 * -30:30/2
-hist(x[abs(x)<1500], breaks=brks, freq=F, col=rgb(0,0,0,0), border=rgb(0,0,0,0), ylim=c(0, 0.005))
-hist(x[abs(x)<1500 & r1.EQ$is.best], breaks=brks, freq=F, col=rgb(1,0,0,0.3), border=rgb(0,0,0,0), add=T, axes=F)
-hist(x[abs(x)<1500 & r1.EQ$is.worst], breaks=brks, freq=F, col=rgb(0,1,0,0.3), border=rgb(0,0,0,0), add=T, axes=F)
-
-# Reject when fragment too short OR no fragment assigned OR distance to site too large
+# -----------------------------------------------------------------------------.
+# Filtering option 1:
+# fragment too short OR no fragment assigned OR distance to site too large
 reject <- with(probes.grg, RF_LEN < 50 | is.na(RF_ID) | RF_DIST>250)
 
+# -----------------------------------------------------------------------------.
+# Filtering option 2:
 # Reject when fragment too short OR no fragment assigned OR rank to site too large
 reject <- with(probes.grg, RF_LEN < 50 | is.na(RF_ID) | RF_RANK>2)
 
-# Reject also probes associated to a poor quality signal in both replicates
+# -----------------------------------------------------------------------------.
+# Always filter out probes associated to a problematic signal in both replicates
 reject <- reject | (r1.EQ$is.worst & r2.EQ$is.worst)
+
+# Make control plot of selected probes (4C signal versus probe distance to site)
+pdf("probeSelection_r1.pdf", width=5, height=5)
+plotSelectedProbes(r1.A, r1.M, dis=probes.grg$RF_DIST, sel = ! reject)
+dev.off()
+pdf("probeSelection_r2.pdf", width=5, height=5)
+plotSelectedProbes(r2.A, r2.M, dis=probes.grg$RF_DIST, sel = ! reject)
+dev.off()
 
 # Update probes and normalized data to retain accepted probes only
 probes.grg <- probes.grg[! reject]
+r1.A <- r1.A[! reject] # For consistency with r1.M and probes.grg
 r1.M <- r1.M[! reject]
+r2.A <- r2.A[! reject] # For consistency with r2.M and probes.grg
 r2.M <- r2.M[! reject]
 
 # =============================================================================.
+# 4C enrichement scores
+# -----------------------------------------------------------------------------.
 # Calculate the number of accepted probes per half-fragment (5’ and 3’ ends)
 probes.grg <- countProbesPerFragment(probes.grg) 
 
@@ -160,6 +188,8 @@ Yi <- y1$SCORE + y2$SCORE
 Yi <- sapply(-2*Yi, pchisq, df=2*2, lower.tail=FALSE, log.p=TRUE)
 
 # =============================================================================.
+# Multi-resolution visualization of 4C enrichement profiles (domainogram)
+# -----------------------------------------------------------------------------.
 # Compute genomic coordinates for visualization
 vis.coords <- visualizationCoordinates(y1$SITE, y1$SITE)
 
@@ -168,7 +198,6 @@ anchors <- read.table(
   "4C_Bait_Sequences/4C_Primers_Aligned_dm6.txt", sep='\t', header=FALSE
 )
 x.bait <- anchors$V4
-
 
 # Define the range of genomic locations to be represented
 xlim <- c(min(vis.coords$start), max(vis.coords$end))
@@ -184,7 +213,9 @@ abline(v=x.bait, col=rgb(1,0,1), lwd=2) # highlight 4C-anchoring location
 dev.off()
 
 # =============================================================================.
-# Identify chromosome interaction domains
+# 4C-enriched chromatin domains
+# -----------------------------------------------------------------------------.
+# Multi-resolution segmentation
 sgm <- segmentation(
   Yi, name="4C_segmentation", wmax=2^12, wmin=5, gamma=1E-4, Tw=-10
 )
@@ -203,12 +234,12 @@ png("4C-enriched_ChromatinDomains_3R.png", width=4096, height=1024)
 plotDomains(doms, vis.coords$start, vis.coords$end, w2y, xlim, wlim, col="black")
 dev.off()
 
-# Visualize max. resolution and max. scale chromosome interaction domains
+# Visualize max. resolution and max. scale domains
 png("4C-enriched_FilteredDomains_3R.png", width=4096, height=1024)
 plotDomains(doms.mr, vis.coords$start, vis.coords$end, w2y, xlim, wlim, col=rgb(0,1,0,0.5), border=rgb(0,0,0,0), lwd=2, lty=1)
 plotDomains(doms.ms, vis.coords$start, vis.coords$end, w2y, xlim, wlim, col=rgb(1,0,0,0.5), border=rgb(0,0,0,0), lwd=2, lty=1, add=T)
 dev.off()
 
-# Retreive genomic coordinates of chromosome interaction domains
+# Retreive genomic coordinates of 4C-enriched chromatin domains
 doms.mr$RS_START <- y1$SITE[doms.mr$start]; doms.mr$RS_END <- y1$SITE[doms.mr$end]
 doms.ms$RS_START <- y1$SITE[doms.ms$start]; doms.ms$RS_END <- y1$SITE[doms.ms$end]
